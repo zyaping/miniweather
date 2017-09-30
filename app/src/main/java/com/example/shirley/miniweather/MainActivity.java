dfe96692566730f03dd6bbf72accf2482dcddb44
zyaping <zhangyaping@pku.edu.cn>
2017-09-30 22:16:49 +0800

    first commit

---
+++ app/src/main/java/com/example/shirley/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.shirley.miniweather;
+
+/**
+ * Created by shirley on 2017/9/28.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
